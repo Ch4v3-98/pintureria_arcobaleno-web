4fe23c6c440f4fa4ebae15d2f9990cd096b6cffa
Ch4v3-98 <ch4v398@gmail.com>
2022-07-18 22:40:55 -0300

    add style imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './styles.css';
+import './App.scss';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
