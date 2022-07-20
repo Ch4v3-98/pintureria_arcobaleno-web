830d171cfc36364d20225aa575a7b78cd88152ad
Ch4v3-98 <ch4v398@gmail.com>
2022-07-20 18:57:55 -0300

    add first version of the brands page

--- src/pages/Marcas.tsx
+++ src/pages/Marcas.tsx
@@ -1,3 +1,28 @@
+import marcas from '../data/marcas';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Heading } from '../components/Heading';
+
 export function Marcas() {
-  return <div>Marcas</div>;
+  return (
+    <Container>
+      <Heading className="mt-5 h5">Marcas</Heading>
+      <Row className="g-3">
+        {marcas.map((marca) => (
+          <Col md={6} lg={4} xl={3}>
+            <Link to="/pinturas">
+              <Card key={marca.id} className="p-5 h-100 hover-shadow bg-light">
+                <Card.Img
+                  variant="top"
+                  src={marca.imagen}
+                  alt={marca.nombre}
+                  style={{ height: '200px', objectFit: 'contain' }}
+                />
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
