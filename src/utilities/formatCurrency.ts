d47e56728429a94ad79487d7a113ad7bd6a37111
Ch4v3-98 <ch4v398@gmail.com>
2022-07-30 23:35:59 -0300

    change formatCurrency helper to use the global currency symbol

--- src/utilities/formatCurrency.ts
+++ src/utilities/formatCurrency.ts
@@ -1,6 +1,7 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+const CURRENCY_FORMATTER = new Intl.NumberFormat('sp', {
   style: 'currency',
   currency: 'ARS',
+  maximumFractionDigits: 2,
 });
 
 export function formatCurrency(number: number) {
