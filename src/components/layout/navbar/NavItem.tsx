a5419437ae13152fa7db25198bc7572d1844e8fa
Ch4v3-98 <ch4v398@gmail.com>
2022-07-31 22:03:36 -0300

    Add a path prop to navItem

--- src/components/layout/navbar/NavItem.tsx
+++ src/components/layout/navbar/NavItem.tsx
@@ -1,19 +1,25 @@
 import { ReactElement } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 type NavItemProps = {
   to: string;
+  path?: string;
   title: string;
   icon: ReactElement;
 };
 
-export function NavItem({ to, title, icon }: NavItemProps) {
+export function NavItem({ to, title, icon, path }: NavItemProps) {
+  const location = useLocation();
+
+  // If the current route contains the path, add the active class
+  const pathMatchs = path ? location.pathname.includes(path) : false;
+
   return (
     <NavLink
       to={to}
       className="nav-item text-decoration-none d-flex align-items-center justify-content-center"
       style={({ isActive }) =>
-        isActive ? { color: '#EFE608' } : { color: 'white' }
+        isActive || pathMatchs ? { color: '#EFE608' } : { color: 'white' }
       }
     >
       {icon}
