6882360cbda02c8227014d135697d4f40d3718ed
Ch4v3-98 <ch4v398@gmail.com>
2022-08-01 21:10:19 -0300

    Improve product page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from 'react';
-import { Container, Row, Col, Image, Stack, Button } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Image,
+  Stack,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { CartPlus } from 'react-bootstrap-icons';
 import { useParams } from 'react-router-dom';
-import { Heading } from '../components/Heading';
-import products from '../data/products';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 import { formatCurrency } from '../utilities/formatCurrency';
+import products from '../data/products';
 
 function Product() {
   const { productId } = useParams();
   const [selectedImage, setImage] = useState('');
+  const { increaseItemQuantity } = useShoppingCart();
 
   const product = products.find((product) => product.id === productId);
 
@@ -26,12 +36,13 @@ function Product() {
   };
 
   return (
-    <Container className="my-5">
+    <Container fluid="lg" className="my-5">
       <Row>
         <div style={{ width: '150px' }}>
           <Stack gap={2}>
             {product.images.map((image) => (
               <Button
+                key={image}
                 variant="outline"
                 className="product-mini-img p-0 bg-white shadow-0"
                 onClick={() => handleImageClick(image)}
@@ -46,7 +57,7 @@ function Product() {
             ))}
           </Stack>
         </div>
-        <Col className="bg-white text-center">
+        <Col md={12} lg={5} className="bg-white text-center my-3 my-lg-0">
           <Image
             fluid
             rounded
@@ -56,11 +67,44 @@ function Product() {
             style={{ height: '500px', objectFit: 'contain' }}
           />
         </Col>
-        <Col md={6}>
-          <Stack className="h-100 rounded bg-light p-5">
-            <Heading className="h3">{product.name}</Heading>
-            <div>{formatCurrency(product.price).slice(0, -3)}</div>
-          </Stack>
+        <Col md={12} lg={4} xl={5}>
+          <Card className="rounded bg-light">
+            <Card.Header className="text-start h4 bg-transparent p-4">
+              {product.name}
+            </Card.Header>
+            <Card.Body>
+              <Card.Text className="d-flex flex-column text-start p-2">
+                <span
+                  style={
+                    product.discount > 0
+                      ? { textDecoration: 'line-through', color: '#868e96' }
+                      : {}
+                  }
+                >
+                  {formatCurrency(product.price)}
+                </span>
+                {product.discount > 0 && (
+                  <span className="fw-bold fs-5">
+                    {' '}
+                    {formatCurrency(
+                      product.price - (product.price * product.discount) / 100,
+                    )}
+                  </span>
+                )}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="d-flex justify-content-between bg-transparent py-3">
+              <Button
+                variant="primary"
+                className="w-50 rounded-pill d-flex align-items-center text-center justify-content-center text-light fw-bold"
+                style={{ padding: '0.60rem 1rem', textTransform: 'none' }}
+                onClick={() => increaseItemQuantity(product.id)}
+              >
+                <CartPlus className="me-2" size={18} />
+                <span style={{ fontSize: '0.85rem' }}>Agregar al carrito</span>
+              </Button>
+            </Card.Footer>
+          </Card>
         </Col>
       </Row>
       <hr className="my-4" />
